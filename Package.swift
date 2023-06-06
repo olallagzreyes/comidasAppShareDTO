40f2d57d15b429fa0b398b3db77f46555e116123
Olalla <olallagzreyes@gmail.com>
2023-06-06 21:07:26 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ComidasAppShareDTO",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ComidasAppShareDTO",
+            targets: ["ComidasAppShareDTO"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ComidasAppShareDTO"),
+        .testTarget(
+            name: "ComidasAppShareDTOTests",
+            dependencies: ["ComidasAppShareDTO"]),
+    ]
+)
